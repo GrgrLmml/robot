c3ee2a2bd571b6439356e3ce8219a840e18dc95b
GrgrLmml <gregor.laemmel@gmail.com>
2019-03-16 21:13:14 +0100

    basic motor control

--- motor.py
+++ motor.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-class Motor():
 
+class MOTOR():
     pwmr = None
 
     def __init__(self):
+        self.M = 10
         GPIO.cleanup()
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(3, GPIO.OUT)
@@ -14,21 +15,18 @@ class Motor():
         self.pwmr = GPIO(7, 100)
         self.pwmr.start(0)
 
-
     def rfwd(self):
         GPIO.output(3, True)
         GPIO.output(5, False)
-
 
     def rrwd(self):
         GPIO.output(3, False)
         GPIO.output(5, True)
 
-
     def rdr(self, spd=50):
         if spd < 0:
             self.rrwd()
-            self.pwmr.ChangeDutyCycle(-1*spd)
+            self.pwmr.ChangeDutyCycle(-1 * spd)
         else:
             self.rfwd()
             self.pwmr.ChangeDutyCycle(spd)
@@ -38,8 +36,7 @@ class Motor():
         GPIO.output(5, False)
         self.pwmr.ChangeDutyCycle(0)
 
-
-m = Motor()
+m = MOTOR()
 
 m.rdr(50)
 sleep(2)
